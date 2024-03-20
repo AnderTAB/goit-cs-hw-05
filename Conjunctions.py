6b3db35febc097d29096e8bc1dbc443b8d8801b3
AnderTAB <111684158+AnderTAB@users.noreply.github.com>
2024-03-20 19:07:18 +0200

    12

---
+++ Conjunctions.py
@@ -0,0 +1,18 @@
+conjunctions = [
+    "and", "but", "or", "so", "yet", "for", "nor", "because", "although", "since",
+    "while", "though", "until", "unless", "provided that", "moreover", "furthermore",
+    "nevertheless", "hence", "thus", "if", "then", "whether", "meanwhile", "therefore",
+    "however", "henceforth", "otherwise", "accordingly", "consequently", "notwithstanding",
+    "inasmuch as", "whilst", "once", "henceforward", "besides", "indeed", "otherwise",
+    "even if", "even though", "on the other hand", "as much as", "rather than", "in case",
+    "as if", "as long as", "in order that", "just as", "insofar as", "only if",
+    "before", "after", "though", "although", "even if", "even though", "while", "whilst",
+    "whereas", "since", "as", "as soon as", "as long as", "till", "until", "once",
+    "lest", "provided that", "so that", "in order that", "unless", "whenever", "wherever",
+    "while", "where", "whether", "whatever", "whoever", "whichever", "however", "but",
+    "rather", "nevertheless", "nonetheless", "instead", "regardless", "otherwise",
+    "therefore", "consequently", "thus", "then", "so", "indeed", "accordingly", "hence",
+    "otherwise", "furthermore", "moreover", "besides", "equally", "similarly", "likewise",
+    "accordingly", "otherwise", "subsequently", "next", "then", "meanwhile", "finally", 
+    "the", "of", "a", "was", "to", "in", "that", "it", "had", "he"
+]
